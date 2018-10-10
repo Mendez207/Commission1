9f7f912c9d72f207e138462e8781d5ad721ff581
Molly Mendez <mollymendez@jh-mba158-25.d214.org>
2018-10-10 11:15:10 -0500

    added outlets

--- Commission1/ViewController.swift
+++ Commission1/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var commisionPayTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
     }
-
-
+    
+    @IBAction func calculateTapped(_ sender: UIButton) {
+        //   getInput()
+    }
+    func getInput() -> Double
+    {
+        if let commisionPay = commisionPayTextField.text, let pay = Double(commisionPay) {
+            print("Has data and can be turned into an integer")
+        } else {
+            print("No data or can't be turned into an integer")
+        }
+        return 13.97
+    }
 }
 
